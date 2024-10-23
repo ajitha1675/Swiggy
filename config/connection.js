106f74449b70cfb46fcfbf11cb0adc049b0c6b05
Ajilalitha <ajilalitha010@gmail.com>
2024-10-23 20:21:18 +0530

    add the connection

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+const mongoose =  require("mongoose");
+
+const connection = () =>{
+    try {
+        mongoose.connect(process.env.MANGO_URL)
+        console.log("Database are connected")
+    } catch (error) {
+        console.log("connection error:", error.message);
+    }
+};
+
+module.exports =  connection;
